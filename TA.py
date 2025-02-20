56b2e9ef63c5521d7a7129044a545a91bbdc5630
Prince Nyarko <princenyarkoedwin@gmail.com>
2025-02-20 11:26:52 +0000

    initial commit

---
+++ TA.py
@@ -0,0 +1,84 @@
+#!/bin/python3
+
+class bNode:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+class Tree:
+    def __init__(self, root=None):
+        self.root = root
+
+    def insert(self, data):
+        if self.root == None:
+            self.root = bNode(data)
+        else:
+            self.insert_builtin(self.root, data)
+
+    def insert_builtin(self, current, data):
+
+        if current.data >= data:
+            if current.left is None:
+                current.left = bNode(data)
+            else:
+                self.insert_builtin(current.left, data)
+        else:
+            if current.right is None:
+                current.right = bNode(data)
+            else:
+                self.insert_builtin(current.right, data)
+
+    def inorder_builtin(self, node):
+        if node:
+            self.inorder_builtin(node.left)
+            print(f"\tData at node: {node.data}")
+            self.inorder_builtin(node.right)
+        return
+
+    def inorder(self):
+        self.inorder_builtin(self.root)
+
+    def preorder_builtin(self, node):
+        if node:
+            print(f"\tData at node: {node.data}")
+            self.preorder_builtin(node.left)
+            self.preorder_builtin(node.right)
+        return
+
+    def preorder(self):
+        self.preorder_builtin(self.root)
+
+    def postorder_builtin(self, node):
+        if node:
+            self.postorder_builtin(node.left)
+            self.postorder_builtin(node.right)
+            print(f"\tData at node: {node.data}")
+        return
+
+    def postorder(self):
+        self.postorder_builtin(self.root)
+
+    def height(self):
+        return self.height_builtin(self.root)
+
+    def height_builtin(self, node):
+        if node is None:
+            return -1
+        a = self.height_builtin(node.left)
+        b = self.height_builtin(node.right)
+        return max(a, b) + 1
+
+if __name__ == "__main__":
+    t1 = Tree()
+    t1.insert(70)
+    t1.insert(89)
+    t1.insert(5)
+    t1.insert(80)
+    t1.insert(51)
+
+    t1.inorder()
+    t1.postorder()
+    t1.preorder()
+    print(t1.height())
---
+++ lab3.py
@@ -0,0 +1,62 @@
+#1825622
+#Nyarko Prince Edwin
+
+class stack_class:
+    def __init__(self,expression):
+        self.expression = expression
+        
+    def is_balanced(self):
+        stack = []
+        mapping = {')':'(',
+                   '}':'{',
+                   ']':'['}
+        
+        for char in self.expression:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or stack.pop() != mapping[char]:
+                    return False
+                
+        return len(stack) == 0
+    
+    def postfix(self):
+        stack = []
+        
+        for char in self.expression.split():
+            if char.isdigit():
+                stack.append(int(char))
+            else:
+                if len(stack) < 2:
+                    raise ValueError("Invalid postfix expression")
+                
+                a = stack.pop()
+                b = stack.pop()
+                
+                if char == '+':
+                    stack.append(a+b)
+                elif char == '-':
+                    stack.append(a-b)
+                elif char == '/':
+                    stack.append(a/b)
+                elif char == '*':
+                    stack.append(a*b)
+        return stack.pop()
+    
+    
+
+expre1 = stack_class("{[()]}")
+print(expre1.is_balanced())
+
+expre2 = stack_class("{[(]}")  
+print(expre2.is_balanced())
+
+expre3 = stack_class("3 4 + 5 * 6 / ")
+print(expre3.postfix())
+
+expre4 = stack_class("10 2 + 4 - ")  
+print(expre4.postfix())             
+                
+        
+    
+                
